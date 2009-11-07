1f879ebb2a6a5c5cba097878dc8246643e65f73c
Balaji T K <balajitk@ti.com>
2009-11-07 02:17:08 +0100

    pad mux changes for i2c1 burst access @100Khz, @400Hz
    Pad mux Pull disabled for I2C1 bus for burst
    	access to work @100Khz and 400Khz
    Signed-off-by: Balaji T K <balajitk@ti.com>

--- board/omap4430sdp/omap4430sdp.c
+++ board/omap4430sdp/omap4430sdp.c
@@ -852,7 +852,7 @@ int dram_init(void)
 	MV(CP(UART2_RX) , ( IEN | M0))  /* uart2_rx */ \
 	MV(CP(UART2_TX) , ( M0))  /* uart2_tx */ \
 	MV(CP(HDQ_SIO) , ( PTD | IEN | M3))  /* gpio_127 */ \
-	MV(CP(I2C1_SCL) , ( PTU | IEN | M0))  /* i2c1_scl */ \
+	MV(CP(I2C1_SCL) , ( IEN | M0))  /* i2c1_scl */ \
 	MV(CP(I2C1_SDA) , ( PTU | IEN | M0))  /* i2c1_sda */ \
 	MV(CP(I2C2_SCL) , ( PTU | IEN | M0))  /* i2c2_scl */ \
 	MV(CP(I2C2_SDA) , ( PTU | IEN | M0))  /* i2c2_sda */ \
@@ -1213,6 +1213,7 @@ int dram_init(void)
 void set_muxconf_regs(void)
 {
 	MUX_DEFAULT_OMAP4();
+	*(volatile int *)(OMAP44XX_CTRL_BASE + CONTROL_I2C_0) = 1; /* Disable Pull */
 	return;
 }
 
