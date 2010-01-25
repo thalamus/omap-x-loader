746f93150545c4d4f3ba359046a5c7249094309a
Balaji T K <balajitk@ti.com>
2010-01-25 19:29:03 +0530

    omap4 mux changes for AUDIO PDM
    omap4 AUDIO PDM mux changes
    Signed-off-by: Balaji T K <balajitk@ti.com>

--- board/omap4430sdp/omap4430sdp.c
+++ board/omap4430sdp/omap4430sdp.c
@@ -839,11 +839,11 @@ int dram_init(void)
 	MV(CP(ABE_MCBSP1_DR) , ( IEN | M1))  /* abe_slimbus1_data */ \
 	MV(CP(ABE_MCBSP1_DX) , ( OFF_EN | OFF_OUT_PTD | M0))  /* abe_mcbsp1_dx */ \
 	MV(CP(ABE_MCBSP1_FSX) , ( IEN | OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_mcbsp1_fsx */ \
-	MV(CP(ABE_PDM_UL_DATA) , ( IEN | OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_pdm_ul_data */ \
-	MV(CP(ABE_PDM_DL_DATA) , ( OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_pdm_dl_data */ \
-	MV(CP(ABE_PDM_FRAME) , ( OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_pdm_frame */ \
-	MV(CP(ABE_PDM_LB_CLK) , ( OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_pdm_lb_clk */ \
-	MV(CP(ABE_CLKS) , ( IEN | OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_clks */ \
+	MV(CP(ABE_PDM_UL_DATA) , ( PTD | IEN | OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_pdm_ul_data */ \
+	MV(CP(ABE_PDM_DL_DATA) , ( PTD | IEN | OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_pdm_dl_data */ \
+	MV(CP(ABE_PDM_FRAME) , ( PTU | IEN | OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_pdm_frame */ \
+	MV(CP(ABE_PDM_LB_CLK) , ( PTD | IEN | OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_pdm_lb_clk */ \
+	MV(CP(ABE_CLKS) , ( PTD | IEN | OFF_EN | OFF_PD | OFF_IN | M0))  /* abe_clks */ \
 	MV(CP(ABE_DMIC_CLK1) , ( M0))  /* abe_dmic_clk1 */ \
 	MV(CP(ABE_DMIC_DIN1) , ( IEN | M0))  /* abe_dmic_din1 */ \
 	MV(CP(ABE_DMIC_DIN2) , ( IEN | M0))  /* abe_dmic_din2 */ \
@@ -852,7 +852,7 @@ int dram_init(void)
 	MV(CP(UART2_RTS) , ( M0))  /* uart2_rts */ \
 	MV(CP(UART2_RX) , ( PTU | IEN | M0))  /* uart2_rx */ \
 	MV(CP(UART2_TX) , ( M0))  /* uart2_tx */ \
-	MV(CP(HDQ_SIO) , ( PTD | IEN | M3))  /* gpio_127 */ \
+	MV(CP(HDQ_SIO) , ( M3))  /* gpio_127 */ \
 	MV(CP(I2C1_SCL) , ( IEN | M0))  /* i2c1_scl */ \
 	MV(CP(I2C1_SDA) , ( PTU | IEN | M0))  /* i2c1_sda */ \
 	MV(CP(I2C2_SCL) , ( PTU | IEN | M0))  /* i2c2_scl */ \
@@ -1073,10 +1073,10 @@ int dram_init(void)
 	MV(CP(ABE_MCBSP1_DX),	(OFF_EN | OFF_OUT_PTD | M0)) /* abe_mcbsp1_dx */ \
 	MV(CP(ABE_MCBSP1_FSX),	(IEN | OFF_EN | OFF_PD | OFF_IN | M0)) /* abe_mcbsp1_fsx */ \
 	MV(CP(ABE_PDM_UL_DATA),	(PTD | IEN | OFF_EN | OFF_PD | OFF_IN | M0_SAFE)) /* abe_pdm_ul_data */ \
-	MV(CP(ABE_PDM_DL_DATA),	(PTD | OFF_EN | OFF_PD | OFF_IN | M0_SAFE)) /* abe_pdm_dl_data */ \
-	MV(CP(ABE_PDM_FRAME),	(PTD | IEN | OFF_EN | OFF_PD | OFF_IN | M0_SAFE)) /* abe_pdm_frame */ \
-	MV(CP(ABE_PDM_LB_CLK),	(OFF_EN | OFF_PD | OFF_IN | M0_SAFE)) /* abe_pdm_lb_clk */ \
-	MV(CP(ABE_CLKS),	(IEN | OFF_EN | OFF_PD | OFF_IN | M0_SAFE)) /* abe_clks */ \
+	MV(CP(ABE_PDM_DL_DATA),	(PTD | IEN | OFF_EN | OFF_PD | OFF_IN | M0_SAFE)) /* abe_pdm_dl_data */ \
+	MV(CP(ABE_PDM_FRAME),	(PTU | IEN | OFF_EN | OFF_PD | OFF_IN | M0_SAFE)) /* abe_pdm_frame */ \
+	MV(CP(ABE_PDM_LB_CLK),	(PTD | IEN | OFF_EN | OFF_PD | OFF_IN | M0_SAFE)) /* abe_pdm_lb_clk */ \
+	MV(CP(ABE_CLKS),	(PTD | IEN | OFF_EN | OFF_PD | OFF_IN | M0_SAFE)) /* abe_clks */ \
 	MV(CP(ABE_DMIC_CLK1),	(M0_SAFE)) /* abe_dmic_clk1 */ \
 	MV(CP(ABE_DMIC_DIN1),	(M0_SAFE)) /* abe_dmic_din1 */ \
 	MV(CP(ABE_DMIC_DIN2),	(M0_SAFE)) /* abe_dmic_din2 */ \
